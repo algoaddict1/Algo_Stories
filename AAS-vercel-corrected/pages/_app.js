f9f4bc3dc54f0b8609bc10b972d2ac9a6e3989c7
algoaddict1 <algoaddictstories@gmail.com>
2025-06-21 09:25:12 +0200

    Update _app.js

--- AAS-vercel-corrected/pages/_app.js
+++ AAS-vercel-corrected/pages/_app.js
@@ -1,5 +1,5 @@
 import { WalletProvider } from "../context/WalletContext";
-
+import '../styles/globals.css'; 
 export default function MyApp({ Component, pageProps }) {
   return (
     <WalletProvider>
